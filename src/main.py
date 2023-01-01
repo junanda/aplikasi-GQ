1144221717486ff1caee7ca927e23e70286d85bf
anzoid <jpatihullah@gmail.com>
2023-01-01 17:57:12 +0700

    registered blueprint route to app

--- src/main.py
+++ src/main.py
@@ -1,8 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from . import auth, dashboard, pred_engine
 
 
-app = Flask()
+app = Flask(__name__)
 app.secret_key = "lansdjkacnkaj1234"
+
+app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://root:triadpass@localhost/gina_cluster'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+app.register_blueprint(auth.auth)
+app.register_blueprint(dashboard.dashboard)
+
+db = SQLAlchemy(app)
 
 if __name__ == "__main__":
     app.run(debug=True)
