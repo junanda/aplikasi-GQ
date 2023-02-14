7337468c08a69fa54964e9866d84a32952b4691f
junanda.patihullah <junanda.patihullah@spesolution.com>
2023-02-14 09:25:29 +0700

    pagination for data pegawai

--- src/blueprint/pegawai.py
+++ src/blueprint/pegawai.py
@@ -2,13 +2,15 @@ from flask import Blueprint, render_template, request, redirect, flash, url_for
 from ..tools import authentication
 from ..model import pegawai
 from ..main import db
+import math
 
 bpegawai = Blueprint('pegawai', __name__, url_prefix='/pegawai')
 
 # index pegawai
 @bpegawai.route('/', methods=('GET','POST'))
+@bpegawai.route('/<pg>', methods=('GET', 'POST'))
 @authentication
-def index():
+def index(pg=0):
     if request.method == 'POST':
         name = request.form['nama_pegawai']
         newPegawai = pegawai.Pegawai(name=name)
@@ -17,8 +19,14 @@ def index():
         flash('New Pegawai successfuly added')
         return redirect(url_for('pegawai.index'))
     l_active = 'active'
-    dataAll = db.engine.execute("select nama_pegawai, id_pegawai from pegawai order by id_pegawai asc")
-    return render_template('data_pegawai.html', active_c=l_active, dataPegawai=dataAll)
+    is_disabled = True
+    dataAll = db.engine.execute(f"select nama_pegawai, id_pegawai from pegawai order by id_pegawai asc limit 15 offset {str(int(pg)*15)}")
+    allRow = db.engine.execute("select * from pegawai").fetchall()
+    totalPage = math.ceil(len(allRow)/15)
+    print(totalPage, len(allRow))
+    if int(pg) > 0 and int(pg) <= totalPage:
+        is_disabled = False
+    return render_template('data_pegawai.html', active_c=l_active, dataPegawai=dataAll, hal=pg, total_page=totalPage, disabled=is_disabled)
 
 
 # edit pegawai
