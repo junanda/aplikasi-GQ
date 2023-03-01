29a272c0777fb4afca877ed47010456ff1a98f68
junanda.patihullah <junanda.patihullah@spesolution.com>
2023-03-01 23:59:36 +0700

    feat:add import excel file using ajax for clustering

--- src/blueprint/clustering_engine.py
+++ src/blueprint/clustering_engine.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request, json, jsonify
+from flask_cors import cross_origin
+from fileinput import filename
 from ..tools import authentication
 from ..main import db
 from sklearn.cluster import KMeans
@@ -10,16 +12,11 @@ clustering = Blueprint('clustering', __name__, url_prefix='/clustering')
 
 #index
 @clustering.route('/', methods=('GET','POST'))
-@clustering.route('/<pg>', methods=('GET','POST'))
 @authentication
-def index(pg=0):
+def index():
     l_active = 'active'
-    disabled = True
-    # query data cluster dengan pagination
-    query = f"select id_aspek, nilai_skp, orientasi, integritas, komitmen, disiplin, kerjasama, label from nilai_aspek_pegawai where label != 'none' limit 20 offset {str(int(pg)*20)}"
     # query untuk mengambil seluruh data nilai_aspek_pegawai
     query2 = "select * from nilai_aspek_pegawai where label !=''"
-    dataPag = db.engine.execute(query).fetchall()
     countRow = db.engine.execute(query2).fetchall()
     
     allData = db.engine.execute("select * from nilai_aspek_pegawai").fetchall()
@@ -27,16 +24,9 @@ def index(pg=0):
     dataCluster1 = db.engine.execute("select * from nilai_aspek_pegawai where label='0'").fetchall()
     dataCluster2 = len(countRow) - len(dataCluster1)
     data_no_label = len(allData) - len(countRow)
-    totalPage = math.ceil(len(countRow)/20)
-    if int(pg) > 0 and int(pg) <= totalPage:
-        disabled = False
     return render_template(
             'clustering_view.html', 
-            active_d=l_active, 
-            data_cluster=dataPag, 
-            hal=pg, 
-            total_page=totalPage, 
-            disabled=disabled,
+            active_d=l_active,
             numberAllData=len(allData),
             dataCluster=len(countRow),
             clus_1=len(dataCluster1),
@@ -63,3 +53,17 @@ def train_clustering():
         db.engine.execute(f"update nilai_aspek_pegawai set label={kmeans.labels_[i]} where id_aspek={data.iloc[i]['id_aspek']}")
     
     return redirect(url_for('clustering.index'))
+
+@clustering.route('/file-excel', methods=['POST'])
+@cross_origin()
+def importFile():
+    resp = any
+    file = request.files['file']
+    data = pd.read_excel(file)
+    data_ex = data.to_json(orient="records")
+    parsed = json.loads(data_ex)
+    print(parsed[1:])
+    resp = jsonify({"message": "success", "data": parsed})
+    resp.status_code = 200
+
+    return resp
