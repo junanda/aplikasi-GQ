2b91290ed86fac82cf9942d3f3865413606a99dd
junanda.patihullah <junanda.patihullah@spesolution.com>
2023-03-03 19:13:22 +0700

    feat: show calculate k-means in web

--- src/blueprint/clustering_engine.py
+++ src/blueprint/clustering_engine.py
@@ -4,11 +4,18 @@ from fileinput import filename
 from ..tools import authentication
 from ..main import db
 from sklearn.cluster import KMeans
-import math
+from scipy.spatial.distance import euclidean
 import pandas as pd
 
 
 clustering = Blueprint('clustering', __name__, url_prefix='/clustering')
+
+centroid = [
+    [88.94495413, 87.55862385, 85.73568807, 85.65174312, 86.28036697, 86.05045872],
+    [85.63931034, 81.24386207, 81.11351724, 80.83731034, 81.03634483, 80.83289655]
+]
+
+col_cal = ['nilai_skp', 'orientasi', 'integritas', 'komitmen', 'disiplin', 'kerjasama']
 
 #index
 @clustering.route('/', methods=('GET','POST'))
@@ -57,13 +64,65 @@ def train_clustering():
 @clustering.route('/file-excel', methods=['POST'])
 @cross_origin()
 def importFile():
+    name_col = ['no','nama', 'nilai_skp', 'orientasi', 'integritas', 'komitmen', 'disiplin', 'kerjasama']
     resp = any
     file = request.files['file']
     data = pd.read_excel(file)
+    data.columns = name_col
     data_ex = data.to_json(orient="records")
     parsed = json.loads(data_ex)
-    print(parsed[1:])
-    resp = jsonify({"message": "success", "data": parsed})
+    resp = jsonify({"message": "success", "data": parsed[1:]})
     resp.status_code = 200
 
     return resp
+
+@clustering.route('/calculate', methods=['POST'])
+def prosesPerhitungan():
+    response = any
+    data = request.get_json() 
+    if data['step'] == 'centroid_1':
+        data_res = calDistancePoint(centroid[0], data['data'])
+        response = jsonify({
+            'message': 'success', 
+            'informasi': 'Hasil perhitungan jarak tiap feature pada data dengan centroid 1',
+            'data':data_res
+        })
+    
+    if data['step'] == 'centroid_2':
+        data_res = calDistancePoint(centroid[1], data['data'])
+        response = jsonify({
+            'message': 'success', 
+            'informasi': 'Hasil perhitungan jarak tiap feature pada data dengan centroid 2',
+            'data':data_res
+        })
+    
+    if data['step'] == 'distance':
+        data_res = calDistanceToCenter(data['data'])
+        response = jsonify({
+            'message': 'success',
+            'informasi': 'Hasil pehitungan jarak antara data dengan centroid cluster',
+            'data': data_res   
+        })
+
+    return response
+
+
+def convDfToJson(df):
+    data_ex = df.to_json(orient="records")
+    return json.loads(data_ex)
+
+def calDistancePoint(cent, data):
+    data_pd = pd.json_normalize(data, max_level=1)
+    data_pd[col_cal] = cent - data_pd[col_cal] 
+    data_ret = convDfToJson(data_pd)
+    return data_ret
+
+def calDistanceToCenter(data):
+    df = pd.json_normalize(data, max_level=1)
+    df['jarak_centroid_0'] = df.apply(lambda r: distance_to_centroid(r, centroid[0]),1)
+    df['jarak_centroid_1'] = df.apply(lambda r: distance_to_centroid(r, centroid[1]),1)
+    return convDfToJson(df)
+
+def distance_to_centroid(row, centroid):
+    row = row[col_cal]
+    return euclidean(row, centroid)
